9386a2712701d4bc4afb3d8b852ac623fc6e760f
Leandro Henrique Perez <leandropremier@gmail.com>
2024-07-28 09:55:04 -0300

    melhorias no runner

--- taf/src/test/java/com/leandroperez/taf/runner/RunMobileTest.java
+++ taf/src/test/java/com/leandroperez/taf/runner/RunMobileTest.java
@@ -8,10 +8,11 @@ import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
-@SelectClasspathResource("com.leandroperez.taf/src/test/resources/features/mobile")
-@ConfigurationParameter(key = Constants.FEATURES_PROPERTY_NAME,value = "src/test/resources/features/mobile/example.feature")
+//@SelectClasspathResource("com.leandroperez.taf/src/test/resources/features/mobile")
+@SelectClasspathResource("features")
+@ConfigurationParameter(key = Constants.FEATURES_PROPERTY_NAME,value = "src/test/resources/features/mobile")
 @ConfigurationParameter(key = Constants.GLUE_PROPERTY_NAME,value = "com/leandroperez/taf/sut/mobile/pom/step")
-@ConfigurationParameter(key = Constants.FILTER_TAGS_PROPERTY_NAME,value = "@SISTEMA")
+@ConfigurationParameter(key = Constants.FILTER_TAGS_PROPERTY_NAME,value = "@SISTEMA and @TAG2")
 @ConfigurationParameter(key = Constants.EXECUTION_DRY_RUN_PROPERTY_NAME,value = "false")
 @ConfigurationParameter(key = Constants.PLUGIN_PROPERTY_NAME,value = "pretty, html:target/cucumber-report/cucumber.html")
 //@IncludeTags({"SISTEMA", "TAG1"})
---
+++ taf/src/test/resources/features/mobile/example2.feature
@@ -0,0 +1,8 @@
+Feature: An example
+
+  @SISTEMA
+  @TAG2
+  Scenario: The example 2
+    Given an example scenario
+    When all step definitions are implemented
+    Then the scenario passes
