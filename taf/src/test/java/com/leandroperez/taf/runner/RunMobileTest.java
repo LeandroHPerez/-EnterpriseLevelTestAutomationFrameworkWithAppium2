1f6c26781da925e069ed21d2807f5c7381d9d971
Leandro Henrique Perez <leandropremier@gmail.com>
2024-07-28 09:39:10 -0300

    Adicionado runner

--- taf/src/test/java/com/leandroperez/taf/runner/RunMobileTest.java
+++ taf/src/test/java/com/leandroperez/taf/runner/RunMobileTest.java
@@ -1,13 +1,19 @@
 package com.leandroperez.taf.runner;
 
 import org.junit.platform.suite.api.*;
+import io.cucumber.junit.platform.engine.Constants;
+
 
 import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
 @Suite
-@SelectClasspathResource("features/mobile")
-@SelectClasspathResource("com/koushick/BDD/Steps")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
-@IncludeTags({"tag1", "tag2"})
+@IncludeEngines("cucumber")
+@SelectClasspathResource("com.leandroperez.taf/src/test/resources/features/mobile")
+@ConfigurationParameter(key = Constants.FEATURES_PROPERTY_NAME,value = "src/test/resources/features/mobile/example.feature")
+@ConfigurationParameter(key = Constants.GLUE_PROPERTY_NAME,value = "com/leandroperez/taf/sut/mobile/pom/step")
+@ConfigurationParameter(key = Constants.FILTER_TAGS_PROPERTY_NAME,value = "@SISTEMA")
+@ConfigurationParameter(key = Constants.EXECUTION_DRY_RUN_PROPERTY_NAME,value = "false")
+@ConfigurationParameter(key = Constants.PLUGIN_PROPERTY_NAME,value = "pretty, html:target/cucumber-report/cucumber.html")
+//@IncludeTags({"SISTEMA", "TAG1"})
 public class RunMobileTest {
 }
--- taf/src/test/resources/features/mobile/example.feature
+++ taf/src/test/resources/features/mobile/example.feature
@@ -1,5 +1,7 @@
 Feature: An example
 
+  @SISTEMA
+  @TAG1
   Scenario: The example
     Given an example scenario
     When all step definitions are implemented
